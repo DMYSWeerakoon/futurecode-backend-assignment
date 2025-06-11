c8523d04004f05e6c72abc44add733cf806304f1
DMYSWeerakoon <yamuweerakoon26@gmail.com>
2025-06-11 16:17:17 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },  // For simplicity, storing plain text (not secure)
+  password: { type: String, required: true },  
 });
 
 module.exports = mongoose.model('User', userSchema);
